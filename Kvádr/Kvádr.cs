f735c0ad2cb8fdc5f837271d918a95942408f9c2
jiri.vondracek <jiri.vondracek@VT3-23>
2023-04-11 12:25:20 +0200

    Přidejte soubory projektu.

---
+++ Kvádr/Kvádr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vypocty
+{
+    internal class Kvádr
+    {
+        public double StranaA { get; set; }
+        public double StranaB { get; set; }
+        public double StranaC { get; set; }
+        public Kvádr(double a, double b, double c)
+        { 
+          this.StranaA = a;
+          this.StranaB = b;
+          this.StranaC = c;
+        }
+        public double VypocetObjemu()
+        {
+            return StranaA *StranaB *StranaC;
+        }
+        public double VypocetPovrchu()
+        {
+            return 2 * (StranaA*StranaB+StranaC*StranaA+StranaB*StranaC);  
+        }
+        public double VypocetTelesUhlo()
+        {
+            return Math.Sqrt( Math.Pow(StranaA, 2) + Math.Pow(StranaB, 2) + Math.Pow(StranaC, 2));
+        }
+        public double VypocetStenUhlo()
+        {
+            return Math.Sqrt( Math.Pow(StranaA, 2) + Math.Pow(StranaB, 2));
+        }
+    }
+}
---
+++ Kvádr/Program.cs
@@ -0,0 +1,10 @@
+﻿using Vypocty;
+double a = Double.Parse(Console.ReadLine());
+double b = Double.Parse(Console.ReadLine());
+double c = Double.Parse(Console.ReadLine());
+Kvádr jmenoObjektu = new Kvádr(a, b, c);
+
+Console.WriteLine(jmenoObjektu.VypocetObjemu());
+Console.WriteLine(jmenoObjektu.VypocetPovrchu());
+Console.WriteLine(jmenoObjektu.VypocetTelesUhlo());
+Console.WriteLine(jmenoObjektu.VypocetStenUhlo());
